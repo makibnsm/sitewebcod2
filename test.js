a2626b9c6087d4264e2af79502b17918a511a623
makibnsm <maxbens02@gmail.com>
2018-07-24 10:08:31 +0200

    On a menu euuuuuuuuuuuh!

--- test.js
+++ test.js
@@ -8,5 +8,12 @@ $(document).ready(function(){
 });
 
 $("#button").click(function(){
+var displayValue = $("#ss-m").css("display")
+console.log(displayValue)
+if (displayValue=="none"){
 $("#ss-m").css("display","block")
-})
+}
+else if  (displayValue=="block"){
+$("#ss-m").css("display","none")
+}
+});
