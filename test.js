f318e20650e3eac088dd9538e2e5df5f0b915e2a
makibnsm <maxbens02@gmail.com>
2018-07-26 12:04:34 +0200

    on conserve le code du ss-menu 2 en comm

--- test.js
+++ test.js
@@ -12,13 +12,13 @@ else if  (displayValue=="block"){
 $("#ss-m").css("display","none")
 }
 });
-$("#cls").click(function(){
-var displayValue = $("#ss-m2").css("display")
-console.log(displayValue)
-if (displayValue=="none"){
-$("#ss-m2").css("display","block")
-}
-else if  (displayValue=="block"){
-$("#ss-m2").css("display","none")
-}
-});
+// $("#cls").click(function(){
+// var displayValue = $("#ss-m2").css("display")
+// console.log(displayValue)
+// if (displayValue=="none"){
+// $("#ss-m2").css("display","block")
+// }
+// else if  (displayValue=="block"){
+// $("#ss-m2").css("display","none")
+// }
+// });
