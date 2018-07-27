ce0d9470f6d422efb9c9760961ec038a7d2c867e
makibnsm <maxbens02@gmail.com>
2018-07-27 21:37:50 +0200

    enfin ça marche youpiii (mm si c moche)

--- test.js
+++ test.js
@@ -14,68 +14,68 @@ $("#ss-m").css("display","none")
 });
 
 // commandes pour les pts de itrmap :
-// $("#c1").click(function(){
-// var displayValue = $("#r1").css("display")
-// console.log(displayValue)
-// if (displayValue=="none"){
-// $("#r1").css("display","block")
-// }
-// else if  (displayValue=="block"){
-// $("#r1").css("display","none")
-// }
-// });
-//
-// $("#c2").click(function(){
-// var displayValue = $("#r2").css("display")
-// console.log(displayValue)
-// if (displayValue=="none"){
-// $("#r2").css("display","block")
-// }
-// else if  (displayValue=="block"){
-// $("#r2").css("display","none")
-// }
-// });
-//
-// $("#c3").click(function(){
-// var displayValue = $("#r3").css("display")
-// console.log(displayValue)
-// if (displayValue=="none"){
-// $("#r3").css("display","block")
-// }
-// else if  (displayValue=="block"){
-// $("#").css("display","none")
-// }
-// });
-//
-// $("#c4").click(function(){
-// var displayValue = $("#r4").css("display")
-// console.log(displayValue)
-// if (displayValue=="none"){
-// $("#r4").css("display","block")
-// }
-// else if  (displayValue=="block"){
-// $("#r4").css("display","none")
-// }
-// });
-//
-// $("#c5").click(function(){
-// var displayValue = $("#r5").css("display")
-// console.log(displayValue)
-// if (displayValue=="none"){
-// $("#r5").css("display","block")
-// }
-// else if  (displayValue=="block"){
-// $("#r5").css("display","none")
-// }
-// });
-//
-// $("#c6").click(function(){
-// var displayValue = $("#r6").css("display")
-// console.log(displayValue)
-// if (displayValue=="none"){
-// $("#r6").css("display","block")
-// }
-// else if  (displayValue=="block"){
-// $("#r6").css("display","none")
-// }
-// });
+$("#c1").click(function(){
+var displayValue = $("#r1").css("display")
+console.log(displayValue)
+if (displayValue=="none"){
+$("#r1").css("display","block")
+}
+else if  (displayValue=="block"){
+$("#r1").css("display","none")
+}
+});
+
+$("#c2").click(function(){
+var displayValue = $("#r2").css("display")
+console.log(displayValue)
+if (displayValue=="none"){
+$("#r2").css("display","block")
+}
+else if  (displayValue=="block"){
+$("#r2").css("display","none")
+}
+});
+
+$("#c3").click(function(){
+var displayValue = $("#r3").css("display")
+console.log(displayValue)
+if (displayValue=="none"){
+$("#r3").css("display","block")
+}
+else if  (displayValue=="block"){
+$("#r3").css("display","none")
+}
+});
+
+$("#c4").click(function(){
+var displayValue = $("#r4").css("display")
+console.log(displayValue)
+if (displayValue=="none"){
+$("#r4").css("display","block")
+}
+else if  (displayValue=="block"){
+$("#r4").css("display","none")
+}
+});
+
+$("#c5").click(function(){
+var displayValue = $("#r5").css("display")
+console.log(displayValue)
+if (displayValue=="none"){
+$("#r5").css("display","block")
+}
+else if  (displayValue=="block"){
+$("#r5").css("display","none")
+}
+});
+
+$("#c6").click(function(){
+var displayValue = $("#r6").css("display")
+console.log(displayValue)
+if (displayValue=="none"){
+$("#r6").css("display","block")
+}
+else if  (displayValue=="block"){
+$("#r6").css("display","none")
+}
+});
